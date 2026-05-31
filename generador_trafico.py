63064af0c61ac9dbff45d698174c7c22db36ad78
Jagsitou <152030582+Jagsitou@users.noreply.github.com>
2026-05-31 20:39:29 +0000

    Integrate Kafka producer for message sending

--- generador_trafico.py
+++ generador_trafico.py
@@ -1,11 +1,21 @@
-import requests
 import time
 import random
 import numpy as np
 import concurrent.futures
+import json
+import uuid
+from datetime import datetime
+from kafka import KafkaProducer
 
-# Se apunta al Caché
-CACHE_URL = "http://localhost:8001"
+# Configuracion para tarea 2 "Inicializacion del productor Kafka"
+print("Conectando con el clúster de Kafka...")
+productor = KafkaProducer(
+    bootstrap_servers=['localhost:9094'],
+    api_version=(3, 7, 0),
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+TOPICO_PRINCIPAL = 'consultas_principal'
+print("¡Productor Kafka listo y conectado!")
 
 # Consultas posibles (Q1, Q2 y Q5 para distintas zonas)
 ZONAS = ["Z1", "Z2", "Z3", "Z4", "Z5"]
@@ -15,27 +25,40 @@ for z in ZONAS:
         f"/api/q1?zona_id={z}&confidence_min=0.6",
         f"/api/q2?zona_id={z}&confidence_min=0.7",
         f"/api/q3?zona_id={z}",
-        f"/api/q4?zona_id={z}",
+        f"/api/q4?zona_a={z}&zona_b=Z1",
         f"/api/q5?zona_id={z}&bins=5"
     ])
 # 15 tipos de consultas distintas.
 
-def hacer_peticion(url):
-    """Función que realiza la llamada HTTP a la API"""
+# Nueva funcion para que se envie a Kafka, que ya no usan HTTP
+def hacer_peticion(consulta_str):
+    """Función que envía el mensaje (evento) a Kafka"""
     try:
-        r = requests.get(url)
-        return r.status_code
-    except Exception:
+        # Estructura
+        mensaje = {
+            "id_consulta": str(uuid.uuid4()),             # Identificador único
+            "retry_count": 0,                             # Número de reintentos
+            "timestamp": datetime.utcnow().isoformat(),   # Timestamp de creación
+            "consulta": consulta_str                      # La ruta de la consulta
+        }
+        
+        # Se envia el mensaje al buzon
+        productor.send(TOPICO_PRINCIPAL, value=mensaje)
+        
+        # 200 en caso de exito
+        return 200 
+    except Exception as e:
+        print(f"Error al enviar a Kafka: {e}")
         return 500
 
 def ejecutar_test(distribucion, total_peticiones):
     print(f"Iniciando bombardeo... Distribución: {distribucion.upper()} ({total_peticiones} peticiones)")
     
-    urls_a_consultar = []
+    consultas_a_enviar = []
     
     if distribucion == "uniforme":
-        # Elegir totalmente al azar
-        urls_a_consultar = [CACHE_URL + random.choice(CONSULTAS_POSIBLES) for _ in range(total_peticiones)]
+        # Elegir totalmente al azar (Ya no le pegamos el CACHE_URL al inicio)
+        consultas_a_enviar = [random.choice(CONSULTAS_POSIBLES) for _ in range(total_peticiones)]
         
     elif distribucion == "zipf":
         # Distribución Zipf: simulando zonas muy populares
@@ -45,27 +68,25 @@ def ejecutar_test(distribucion, total_peticiones):
         for idx in indices:
             # Ajustamos el índice para que no se salga de nuestra lista de 15 consultas
             indice_ajustado = (idx - 1) % len(CONSULTAS_POSIBLES)
-            urls_a_consultar.append(CACHE_URL + CONSULTAS_POSIBLES[indice_ajustado])
+            consultas_a_enviar.append(CONSULTAS_POSIBLES[indice_ajustado])
 
     # Iniciar cronómetro
     inicio = time.time()
     
-    # ThreadPoolExecutor: Peticiones simultaneas
-    hilos_simultaneos = 10 
+    # ThreadPoolExecutor: Peticiones concurrentes
     exitos = 0
-    with concurrent.futures.ThreadPoolExecutor(max_workers=hilos_simultaneos) as executor:
-        resultados = list(executor.map(hacer_peticion, urls_a_consultar))
-        exitos = resultados.count(200) # Contador de '200 OK'
-
-    # Detener cronómetro
+    fallos = 0
+    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+        resultados = list(executor.map(hacer_peticion, consultas_a_enviar))
+        
+    for r in resultados:
+        if r == 200:
+            exitos += 1
+        else:
+            fallos += 1
+            
     fin = time.time()
-    tiempo_total = fin - inicio
-    rendimiento = total_peticiones / tiempo_total
-    
-    # Imprimir métricas
-    print(f"Exitosas: {exitos}/{total_peticiones}")
-    print(f"Tiempo total: {tiempo_total:.2f} segundos")
-    print(f"Rendimiento: {rendimiento:.2f} peticiones/segundo\n")
+    print(f"Terminado en {fin - inicio:.2f}s | Éxitos: {exitos} | Fallos: {fallos}\n")
 
 if __name__ == "__main__":
     print("######################################  Iniciando... ###################################### \n")
@@ -76,3 +97,8 @@ if __name__ == "__main__":
     
     # Ronda 2: Tendencia realista (100 peticiones)
     ejecutar_test(distribucion="zipf", total_peticiones=100)
+    
+    # EL COMANDO MÁGICO: Obligar a vaciar la RAM y enviar todo al servidor de Docker
+    print("Vaciando búfer y asegurando la entrega a Kafka...")
+    productor.flush()
+    print("¡Todos los mensajes han sido forzados a salir y entregados a Kafka exitosamente!")
